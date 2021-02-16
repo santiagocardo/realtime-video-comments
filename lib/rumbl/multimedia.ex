9055129dae664eee7e9d053ec60745a960c0e6be
Santiago Cardona <santiagocardo80@gmail.com>
2021-02-16 11:20:18 -0500

    💾 Ecto: add constraints and list caterories functionallity

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -9,6 +9,18 @@ defmodule Rumbl.Multimedia do
   alias Rumbl.Multimedia.Video
   alias Rumbl.Accounts
 
+  alias Rumbl.Multimedia.Category
+
+  def create_category!(name) do
+    Repo.insert!(%Category{name: name}, on_conflict: :nothing)
+  end
+
+  def list_alphabetical_categories do
+    Category
+    |> Category.alphabetical()
+    |> Repo.all()
+  end
+
   @doc """
   Returns the list of videos.
 
